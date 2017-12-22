544cc80e96c746268d2297ad097490e22ba623af
Omaroovee <Muhammad.omar.eg@gmail.com>
2017-12-22 15:21:20 +0200

    complete the TweetingBot

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ var TWEET = new Twit(config);
 tweetIt();
 
 // this make the bot tweet every 24 Hours
-setInterval(tweetIt, 1000*20);
+setInterval(tweetIt, 1000*60*60*24);
 
 function tweetIt() {
 
