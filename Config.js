b248e8a750455fedb8f03e5662638a7ede85d9d5
Omaroovee <Muhammad.omar.eg@gmail.com>
2017-12-22 12:21:53 +0200

    make the app read from file and tweet what it reads

--- Config.js
+++ Config.js
@@ -5,8 +5,8 @@
 // add  your  Key and Token
 // from https://apps.twitter.com/
 module.exports = {
-    consumer_key:         '',
-    consumer_secret:      '',
-    access_token:         '',
-    access_token_secret:  ''
+    consumer_key:         'xWcRWz6QBHQRdi9opAVV7GWCN',
+    consumer_secret:      'cUhOxSRFGaIt8m7RdUkz7IPoXodTKJzYG7f5wae53AREssY1ZN',
+    access_token:         '3498057921-siQzHd0Fo1G7UbNI5JpA2KCxhOhOOBsIkazXkah',
+    access_token_secret:  'n1kNuWDSEW5CcYt2gCXQ4apvSmBSRJwiCWznRflbNlh8i'
 }
--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 console.log('It,worked');
 
-// importing the Twitter Module
-var Twit = require('twit');
 
-// import the outh Module
-var config = require('./Config');
-console.log(config);
+var Twit = require('twit'); // importing the Twitter Module
+const randomFile = require('select-random-file') // importing the randomFile Module
+var config = require('./Config'); // importing the outh Module
+var fs = require('fs'); // importing files Module
+
+var Msg ;
 
 var TWEET = new Twit(config);
-
 tweetIt();
 
 // this make the bot tweet every 24 Hours
@@ -16,18 +16,23 @@ setInterval(tweetIt, 1000*20);
 
 function tweetIt() {
 
-// TODO : choose a random message
-	var tweet = {
-	  status: ' بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ  '  + Msg +' صدق اللَّهُ العظيم ' + ' #TwitterBot'
-	}
 
-	TWEET.post('statuses/update', tweet, tweeted);
 
-	function tweeted(err, data, response) {
-	  if (err) {
-	  	console.log("There is A problem");
-	  } else {
-	    console.log("It worked!");
-	  }
-	}
+	fs.readFile('./Messages/1.txt', 'utf8', function(err, data) {
+			if (err) throw err;
+			var tweet = {
+				status: '   بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ  '  + data +'  صدق اللَّهُ العظيم   ' + ' #TwitterBot'
+			}
+			TWEET.post('statuses/update', tweet, tweeted);
+
+			function tweeted(err, data, response) {
+			  if (err) {
+			  	console.log("There is A problem");
+			  } else {
+			    console.log("It worked!");
+			  }
+			}
+	});
+
 }
+console.log('Tweeted');
