ceb2d426cd7b747cd64d6646b41478a3f27b626f
Omaroovee <Muhammad.omar.eg@gmail.com>
2017-12-22 10:30:52 +0200

    complet the tweet function and make it every 24 hours , add todo task

--- bot.js
+++ bot.js
@@ -7,4 +7,27 @@ var Twit = require('twit');
 var config = require('./Config');
 console.log(config);
 
-var Tweet = new Twit(config);
+var TWEET = new Twit(config);
+
+tweetIt();
+
+// this make the bot tweet every 24 Hours
+setInterval(tweetIt, 1000*60*60*24);
+
+function tweetIt() {
+
+// TODO : choose a random message
+	var tweet = {
+	  status: ' بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ  '  + Msg +' صدق اللَّهُ العظيم ' + ' #TwitterBot'
+	}
+
+	TWEET.post('statuses/update', tweet, tweeted);
+
+	function tweeted(err, data, response) {
+	  if (err) {
+	  	console.log("There is A problem");
+	  } else {
+	    console.log("It worked!");
+	  }
+	}
+}
