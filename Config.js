d25ceac017d72015a76cdf29c6af319540ec08ae
Omaroovee <Muhammad.omar.eg@gmail.com>
2017-12-23 10:03:39 +0200

    clean up

--- Config.js
+++ Config.js
@@ -1,12 +1,11 @@
-// this is  outh Module
-
+// this is  oAuth Module
 
 
 // add  your  Key and Token
 // from https://apps.twitter.com/
 module.exports = {
-    consumer_key:         'xWcRWz6QBHQRdi9o..',
-    consumer_secret:      'cUhOxSRFGaIt8m7RdUkz7IdTKJzYG7f5wae53AREssY1ZN....',
-    access_token:         '3498057921-siQzHd0Fo1G7UbNI5JpA2KC....',
-    access_token_secret:  'n1kNuWDSEW5CcYt2gCXQ4apvSmBSRflbNlh8i....'
+  consumer_key: 'xWcRWz6QBHQRdi9o..',
+  consumer_secret: 'cUhOxSRFGaIt8m7RdUkz7IdTKJzYG7f5wae53AREssY1ZN....',
+  access_token: '3498057921-siQzHd0Fo1G7UbNI5JpA2KC....',
+  access_token_secret: 'n1kNuWDSEW5CcYt2gCXQ4apvSmBSRflbNlh8i....'
 }
--- bot.js
+++ bot.js
@@ -1,40 +1,31 @@
-console.log('It,worked');
-
-
 var Twit = require('twit'); // importing the Twitter Module
-var config = require('./Config'); // importing the outh Module
+var config = require('./Config'); // importing the oAuth Module
 var fs = require('fs'); // importing files Module
 
-var Msg ;
 
 var TWEET = new Twit(config);
-tweetIt();
 
-// this make the bot tweet every 24 Hours
-setInterval(tweetIt, 1000*60*60*24);
+tweetIt();
+setInterval(tweetIt, 1000 * 60 * 60 * 24); // this make the bot tweet every 24 Hours
 
 function tweetIt() {
 
-var r = Math.floor(Math.random()*49) + 1;
+  var r = Math.floor(Math.random() * 49) + 1; // generate a random number between 1 and 50
 
-	fs.readFile('./Messages/'+r+'.txt', 'utf8', function(err, data) {
+  fs.readFile('./Messages/' + r + '.txt', 'utf8', function(err, data) { // to read the file
+    if (err) throw err;
+    var tweet = { // the tweet content
+      status: '   بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ  ' + data + '  صدق اللَّهُ العظيم   ' + ' #TweetingBot'
+    }
+    TWEET.post('statuses/update', tweet, tweeted); // the request to make the tweets
 
+    function tweeted(err, data, response) { // a test function check if the tweet is tweeted or not
+      if (err) {
+        console.log("There is A problem");
+      } else {
+        console.log("It worked!");
 
-			if (err) throw err;
-			var tweet = {
-				status: '   بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ  '  + data +'  صدق اللَّهُ العظيم   ' + ' #TweetingBot'
-			}
-			TWEET.post('statuses/update', tweet, tweeted);
-
-			function tweeted(err, data, response) {
-			  if (err) {
-			  	console.log("There is A problem");
-			  } else {
-			    console.log("It worked!");
-
-			  }
-			}
-	});
-
+      }
+    }
+  });
 }
-console.log('Tweeted');
